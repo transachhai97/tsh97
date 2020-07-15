b19ae36caff90a1f1ff9b35493c0e086869ff3e3
HaiTS <transachhai97@gmail.com>
2020-07-15 22:45:52 +0700

    pug

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,17 +1,11 @@
-<template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+<template lang="pug">
+    .container
+        .row.justify-content-center
+            .col-md-8
+                .card
+                    .card-header Example Component
+                    .card-body
+                        | I'm an example component.
 </template>
 
 <script>
