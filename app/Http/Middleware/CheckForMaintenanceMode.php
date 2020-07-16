27e00d3888f6e58d86fc8799e158b2f14377fafa
HaiTS <transachhai97@gmail.com>
2020-07-16 23:37:37 +0700

    maintenance mode

--- app/Http/Middleware/CheckForMaintenanceMode.php
+++ app/Http/Middleware/CheckForMaintenanceMode.php
@@ -2,10 +2,19 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
 use Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode as Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Route;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class CheckForMaintenanceMode extends Middleware
 {
+    /**
+     * @var array
+     */
+    protected $excludedNames = [];
+
     /**
      * The URIs that should be reachable while maintenance mode is enabled.
      *
@@ -13,5 +22,63 @@ class CheckForMaintenanceMode extends Middleware
      */
     protected $except = [
         //
+        'maintenance/*'
     ];
+
+    /**
+     * @var array
+     */
+    protected $excludedIPs = [];
+
+    /**
+     * @param Request $request
+     * @param Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($this->app->isDownForMaintenance()) {
+            $response = $next($request);
+
+            if (in_array($request->ip(), $this->excludedIPs)) {
+                return $response;
+            }
+
+            $route = $request->route();
+
+            if ($route instanceof Route) {
+                if (in_array($route->getName(), $this->excludedNames)) {
+                    return $response;
+                }
+            }
+
+            if ($this->shouldPassThrough($request)) {
+                return $response;
+            }
+
+            throw new HttpException(503);
+        }
+
+        return $next($request);
+    }
+
+    /**
+     * @param $request
+     * @return bool
+     */
+    protected function shouldPassThrough($request)
+    {
+        foreach ($this->except as $except) {
+            if ($except !== '/') {
+                $except = trim($except, '/');
+            }
+
+            if ($request->is($except)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('maintenance/{mode}', 'MaintenanceController')
+    ->where('mode', 'up|down');
+
 Route::get('/{any}', 'SpaController')
     ->where('any', '.*');
