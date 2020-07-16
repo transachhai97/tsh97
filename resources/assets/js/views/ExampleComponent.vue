ff5585ff9f33287b21f415b019a22136fa69c2a8
HaiTS <transachhai97@gmail.com>
2020-07-16 14:23:15 +0700

    pug

---
+++ resources/assets/js/views/ExampleComponent.vue
@@ -0,0 +1,9 @@
+<template lang="pug" src="../templates/example-component.pug"></template>
+
+<script>
+export default {
+    mounted() {
+        console.log('Component mounted.');
+    },
+};
+</script>
