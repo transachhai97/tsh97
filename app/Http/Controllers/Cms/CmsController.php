6dbbbd0c2391049c8dedf0a2edd1311ceca99a92
HaiTS <transachhai97@gmail.com>
2020-07-17 08:58:23 +0700

    cms r

---
+++ app/Http/Controllers/Cms/CmsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Cms;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CmsController extends Controller
+{
+    //
+    public function index()
+    {
+        return 'CMS';
+    }
+}
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -8,20 +8,24 @@ use Illuminate\Support\Facades\Route;
 class RouteServiceProvider extends ServiceProvider
 {
     /**
+     * The path to the "home" route for your application.
+     *
+     * @var string
+     */
+    public const HOME = '/home';
+    /**
      * This namespace is applied to your controller routes.
      *
      * In addition, it is set as the URL generator's root namespace.
      *
      * @var string
      */
-    protected $namespace = 'App\Http\Controllers';
+    protected $namespace = 'App\Http\Controllers\Web';
 
     /**
-     * The path to the "home" route for your application.
-     *
      * @var string
      */
-    public const HOME = '/home';
+    protected $namespaceCms = 'App\Http\Controllers\Cms';
 
     /**
      * Define your route model bindings, pattern filters, etc.
@@ -44,23 +48,10 @@ class RouteServiceProvider extends ServiceProvider
     {
         $this->mapApiRoutes();
 
+        // CMS
+        $this->mapCmsRoutes();
+
         $this->mapWebRoutes();
-
-        //
-    }
-
-    /**
-     * Define the "web" routes for the application.
-     *
-     * These routes all receive session state, CSRF protection, etc.
-     *
-     * @return void
-     */
-    protected function mapWebRoutes()
-    {
-        Route::middleware('web')
-            ->namespace($this->namespace)
-            ->group(base_path('routes/web.php'));
     }
 
     /**
@@ -76,5 +67,29 @@ class RouteServiceProvider extends ServiceProvider
             ->middleware('api')
             ->namespace($this->namespace)
             ->group(base_path('routes/api.php'));
+    }
+
+    /**
+     * @return void
+     */
+    protected function mapCmsRoutes()
+    {
+        Route::prefix('cms')
+            ->namespace($this->namespaceCms)
+            ->group(base_path('routes/cms.php'));
+    }
+
+    /**
+     * Define the "web" routes for the application.
+     *
+     * These routes all receive session state, CSRF protection, etc.
+     *
+     * @return void
+     */
+    protected function mapWebRoutes()
+    {
+        Route::middleware('web')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/web.php'));
     }
 }
