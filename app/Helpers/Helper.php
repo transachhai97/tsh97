a9225cfbf8610968539977c92e73e3b27a32be92
HaiTS <transachhai97@gmail.com>
2020-07-20 16:01:20 +0700

    php doc

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -1,6 +1,16 @@
 <?php
 
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\View\View;
+
 if (!function_exists('viewCms')) {
+    /**
+     * @param null $view
+     * @param array $data
+     * @param array $mergeData
+     * @return Application|Factory|View
+     */
     function viewCms($view = null, $data = [], $mergeData = [])
     {
         return view('cms.' . $view, $data, $mergeData);
